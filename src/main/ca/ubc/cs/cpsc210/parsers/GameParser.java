868de4659a39d4480ff31e8fd17ec408ec1de02b
p2u1b <p2u1b@ugrad.cs.ubc.ca>
2019-03-19 00:18:13 -0700

    Json parsing 70%

--- src/main/ca/ubc/cs/cpsc210/parsers/GameParser.java
+++ src/main/ca/ubc/cs/cpsc210/parsers/GameParser.java
@@ -1,11 +1,16 @@
 package ca.ubc.cs.cpsc210.parsers;
 
 import ca.ubc.cs.cpsc210.model.Game;
+import org.json.JSONObject;
 
 public class GameParser {
 
     public static Game parse(String input) {
         Game game = new Game("s");
+        JSONObject saveGame = new JSONObject(input);
         return game;
+
     }
+
+
 }
