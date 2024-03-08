1d7b0a8ac5cd3e33b3f9e63a968313d7efb88bcc
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-03-09 01:40:15 +0600

    added new javafx library and upgraded project to java 21

--- src/main/ca/ubc/cs/cpsc210/ui/CloseGame.java
+++ src/main/ca/ubc/cs/cpsc210/ui/CloseGame.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 public class CloseGame {
 
     private static void closeProgram(Stage window) {
-        Boolean closeNow = ConfirmBox.display("", "Are you sure you want to exit?");
+        boolean closeNow = ConfirmBox.display("", "Are you sure you want to exit?");
         if (closeNow) {
             window.close();
 
